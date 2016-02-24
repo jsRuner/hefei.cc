3634d2b7a0e465491f23852c799a2348a58b66e2
jsRuner <hi_php@163.com>
2016-02-24 17:15:03 +0800

    save

---
+++ reply.py
@@ -0,0 +1,250 @@
+# -*- coding: UTF-8 -*-
+import urllib2, urllib, cookielib, re, time,json
+import random
+import sqlite3
+import threading
+import ConfigParser
+
+class Robot:
+
+    def __init__(self, forumUrl, userName, password, proxy = None):
+
+        self.forumUrl = forumUrl
+        self.userName = userName
+        self.password = password
+        self.token = ''
+        self.fid = 297
+        self.isLogon = False
+
+
+        self.jar = cookielib.CookieJar()
+        if not proxy:
+            openner = urllib2.build_opener(
+        urllib2.HTTPCookieProcessor(self.jar))
+        else:
+            openner = urllib2.build_opener(
+            urllib2.HTTPCookieProcessor(self.jar),
+            urllib2.ProxyHandler({'http' : proxy}))
+        urllib2.install_opener(openner)
+
+    def login(self):
+        request = urllib2.Request("http://newapp.hefei.cc/mv3_user_login")
+
+        request.add_header('Host', 'newapp.hefei.cc')
+        request.add_header("User_Agent","Android client")
+        request.add_header("Cookie","PHPSESSID=1g9hf0vrv59bb8v2mbuo4mb677")
+        request.add_header("Mag-Deviceid","860076030620446")
+        request.add_header("Mag-Mgsc","31d0477d5d506e967a8614b890636905")
+
+        data = {
+            "device":860076030620446,
+            "build":"3.1.0",
+            "ulng":"4.9E-324",
+            "version":136,
+            "name":self.userName,
+            "clienttype":"android",
+            "ulat":"4.9E-324",
+            "pswd":self.password,
+        }
+
+
+        data = urllib.urlencode(data)
+        # response = urllib.urlopen(request,data)
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+        response = opener.open(request,data)
+        content = response.read()
+        if self.userName in content:
+            self.isLogon = True
+            #解析json。
+            result = json.loads(content)
+            self.token = result['token']
+            print(u"--------------登录信息------------------")
+            for item in result.keys():
+                print "|    %s:%s   " % (item,result[item])
+            print(u"--------------登录信息------------------")
+
+            print 'login success!'
+        else:
+            print 'login faild!'
+    #获取主题列表。json.
+    def getTids(self):
+        # self.token = "7ade721c39fcb6bd9d8da3f28149147c"
+        # self.fid = 297
+        baselink = "http://newapp.hefei.cc/mv3_bbs_contentlist"
+        # link =  "http://newapp.hefei.cc/mv3_bbs_contentlist?step=20&_token="+self.token+"&build=3.1.0&timeline=1455867980&p="+p+"&version=135&fid=297&clienttype=android&"
+        data = {
+            "step":20,
+            "_token":self.token,
+            "build":"3.1.0",
+            "timeline":int(time.time()),
+            "p":1,
+            "version":20,
+            "fid":self.fid,
+            "clienttype":"android",
+        }
+        link = baselink+"?"+urllib.urlencode(data)
+        # print link
+        tidPage = urllib2.urlopen(link).read()
+        result = json.loads(tidPage)
+        # result2 = json.loads(result['list'])
+        tids =[]
+        for item in result['list']:
+            # print item
+            # print item['id']
+            # result_item = json.loads(item)
+            # print result_item['id']
+            tids.append(item['id'])
+        return tids
+    #获取所有板块fid集合
+    #http://newapp.hefei.cc/mv3_bbs_grouplist?step=20&_token=7ade721c39fcb6bd9d8da3f28149147c&build=3.1.0&clienttype=android&p=1&version=135&
+    def getFids(self):
+        baselink = "http://newapp.hefei.cc/mv3_bbs_grouplist"
+        data = {
+            "step":20,
+            "_token":self.token,
+            "build":"3.1.0",
+            "p":1,
+            "version":135,
+        }
+        link = baselink+"?"+urllib.urlencode(data)
+        # print link
+        fidPage = urllib2.urlopen(link).read()
+        result = json.loads(fidPage)
+        fids =[]
+        for item in result['data']:
+            for item_child in item['_child']:
+                # print item_child
+                #不存在则插入
+                if item_child['id'] not in fids:
+                    fids.append(item_child['id'])
+        return fids
+
+    def getOneTitle(self):
+        result = urllib2.urlopen(self.forumUrl + '/forum.php')
+        reglink = r"href=\"(http\:\/\/chizhouren\.com\/forum\.php\?mod=viewthread&tid=6796.+?)\""
+        link = re.findall(re.compile(reglink),result.read())
+        print link[0]
+        return link[0]
+
+    def getTid(self,link):
+        tidPage=urllib2.urlopen(link).read()
+        tidnum=re.search('\d{6}',link)
+        print tidnum.group()
+        return tidnum.group()
+
+    def getFid(self,link):
+        tidPage=urllib2.urlopen(link).read()
+        fid =re.search('<input\s*type="hidden"\s*name="srhfid"\s*value="([\w\W]+?)"\s*\/>',tidPage)
+        print fid.group(1)
+        return fid.group(1)
+
+    def initFormhashXq(self):
+        content = urllib2.urlopen(self.forumUrl + '/plugin.php?id=dsu_paulsign:sign')
+        content = content.read()
+        rows = re.findall(r'<input\s*type="hidden"\s*name="formhash"\s*value="([\w\W]+?)"\s*\/>', content)
+        if len(rows)!=0:
+            self.formhash = rows[0]
+            print 'formhash is: ' + self.formhash
+        else:
+            print 'none formhash!'
+
+    def reply(self,tid,msg):
+        html = ''
+        request = urllib2.Request("http://newapp.hefei.cc/mv3_bbs_commentadd")
+        request.add_header('Host', 'newapp.hefei.cc')
+        request.add_header("User_Agent","Android client")
+        request.add_header("Cookie","PHPSESSID=1g9hf0vrv59bb8v2mbuo4mb677")
+        request.add_header("Mag-Deviceid","860076030620446")
+        request.add_header("Mag-Mgsc","31d0477d5d506e967a8614b890636905")
+        data = {
+            "_token":self.token,
+            "build":"3.1.0",
+            "contentid":tid,
+            "version":135,
+            "clienttype":"android",
+            "commentid":0,
+            "content":msg,
+        }
+        data = urllib.urlencode(data)
+        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+        response = opener.open(request,data)
+        html = response.read().decode("gbk")
+        print("%s" % html)
+
+        print 'reply success!'
+
+def loop():
+    cf = ConfigParser.ConfigParser()
+    cf.read('config.ini')
+    username = cf.get("info","username")
+    password = cf.get("info","password")
+    speed = cf.get("info","speed")
+    robot = Robot('http://bbs.hefei.cc', username, password)
+    robot.login()
+    replylist = [
+            u'不错，支持一下.......',
+            u'已阅，顶一下.......',
+            u'顶一个...........',
+            u'路过帮顶........',
+            u'沙发，沙发.....',
+            u'我的沙发........',
+            u'我来了.........',
+            u'沙发是我的......',
+            u'我来看看.......',
+            u'前排，前排........'
+    ]
+    #随机获取一些帖子。进行回复。回复一个，就删除一个。如果回复完了，再次获取。
+    fids = robot.getFids()
+    #设置fid
+    robot.fid = random.choice(fids)
+    #获取tids
+    tids = robot.getTids()
+    for item in tids:
+        print item
+        print "http://bbs.hefei.cc/thread-"+item+"-1-1.html"
+        content = random.choice(replylist)
+        content = content.encode('utf-8')
+        robot.reply(item,content)
+        #20秒后再回复。
+        time.sleep(speed)
+    #递归调用自身.
+    loop()
+
+if __name__ == '__main__':
+    loop()
+    # cf = ConfigParser.ConfigParser()
+    # cf.read('config.ini')
+    # username = cf.get("info","username")
+    # password = cf.get("info","password")
+    # speed = cf.get("info","speed")
+    # robot = Robot('http://bbs.hefei.cc', username, password)
+    # robot.login()
+    # replylist = [
+    #         u'不错，支持一下.......',
+    #         u'已阅，顶一下.......',
+    #         u'顶一个...........',
+    #         u'路过帮顶........',
+    #         u'沙发，沙发.....',
+    #         u'我的沙发........',
+    #         u'我来了.........',
+    #         u'沙发是我的......',
+    #         u'我来看看.......',
+    #         u'前排，前排........'
+    # ]
+    # #随机获取一些帖子。进行回复。回复一个，就删除一个。如果回复完了，再次获取。
+    # fids = robot.getFids()
+    # #设置fid
+    # robot.fid = random.choice(fids)
+    # #获取tids
+    # tids = robot.getTids()
+    # for item in tids:
+    #     print item
+    #     print "http://bbs.hefei.cc/thread-"+item+"-1-1.html"
+    #     content = random.choice(replylist)
+    #     content = content.encode('utf-8')
+    #     robot.reply(item,content)
+    #     #20秒后再回复。
+    #     time.sleep(speed)
+
+
+
